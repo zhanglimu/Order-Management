aecf4a94eb90766e8f774ab438290a9449c61b0c
zhangye <1756925172@qq.com>
2018-05-02 17:58:41 +0800

    all

--- src/app/const/app-config.ts
+++ src/app/const/app-config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     // static baseUrl: string = 'http://private-489ac-caiexomsapi.apiary-mock.com';
-    // static baseUrl: string = 'http://192.168.1.217:8080/caiex-oms';//1.5
+    static baseUrl: string = 'http://192.168.1.217:8080/caiex-oms';//1.5
     // static baseUrl: string = 'http://oms-aws.jingcai.trade:12377/caiex-oms';//云测试
-    static baseUrl: string = 'http://oms-service.caiex.com:12377/caiex-oms';//生产
+    // static baseUrl: string = 'http://oms-service.caiex.com:12377/caiex-oms';//生产
 }
